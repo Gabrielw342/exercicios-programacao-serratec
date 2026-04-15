ff825c05a566f177672dfec4a25b00ef09dfaae5
Just some dude <145404207+Gabrielw342@users.noreply.github.com>
2026-04-15 17:54:06 -0300

    Update exercicio 3.itempedido.java

--- atividades1-10/exercicio 3/exercicio 3.itempedido.java
+++ atividades1-10/exercicio 3/exercicio 3.itempedido.java
@@ -1,22 +1,21 @@
 //so botando o codigo inicial eu ainda tenho que consertar mas isso e problema do gabriel do futuro
-public class itempedido {
+//bom eu sou o gabriel do futuro e eu tive que trocar o nome da classe pra letras maiscula poque tava dando erro e troque o get nome
+public class ItemPedido {
 
     private String descricao;
     private int quantidade;
-    private double  precounitario;
+    private double precounitario;
 
-    public itempedido(String descricao, int quantidade, double precounitario) {
+    public ItemPedido(String descricao, int quantidade, double precounitario) {
         if (quantidade <= 0 || precounitario <= 0)
-
             throw new IllegalArgumentException("Quantidade ou preço inválidos");
 
         this.descricao = descricao;
         this.quantidade = quantidade;
         this.precounitario = precounitario;
-
     }
 
-    public double getsubtotal() {
+    public double getSubtotal() {
         return quantidade * precounitario;
     }
 }
