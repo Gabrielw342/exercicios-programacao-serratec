1d88efd5d2a086ab1930f22ce922549911fbd39c
Just some dude <145404207+Gabrielw342@users.noreply.github.com>
2026-04-15 17:52:36 -0300

    Update exercicio 3.cliente.java

--- atividades1-10/exercicio 3/exercicio 3.cliente.java
+++ atividades1-10/exercicio 3/exercicio 3.cliente.java
@@ -1,11 +1,16 @@
 //so botando o codigo inicial eu ainda tenho que consertar mas isso e problema do gabriel do futuro
+//bom eu sou o gabriel do futuro e eu conserte o nome cliente pra Cliente porque a letra minuscula tava dando problema
+public class Cliente {
 
-public class cliente {
     private String nome;
 
-    public cliente(String nome) {
+    public Cliente(String nome) {
         if (nome == null || nome.isEmpty())
             throw new IllegalArgumentException("nome invalidade tente novaamentr");
         this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
     }
 }
