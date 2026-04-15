334aecc4517a5c4c2bafbf3f77d424fa9144058a
Just some dude <145404207+Gabrielw342@users.noreply.github.com>
2026-04-15 17:56:58 -0300

    Update exercicio 3.main.java

--- atividades1-10/exercicio 3/exercicio 3.main.java
+++ atividades1-10/exercicio 3/exercicio 3.main.java
@@ -1,17 +1,18 @@
 //so adicionando o codigo depois eu conserto
+//o mais facil de consertar so trocar algumas letras minusculas
 public class MainAtividade3 {
 
     public static void main(String[] args) {
 
         try {
-            cliente cliente = new cliente("Gabriel");
-            pedido pedido = new pedido(cliente);
+            Cliente cliente = new Cliente("Gabriel");
+            Pedido pedido = new Pedido(cliente);
 
-            pedido.adicionarItem(new itempedido("notebook gamer", 1, 8200));
-            pedido.adicionarItem(new itempedido("mouse", 2, 50));
-            pedido.adicionarItem(new itempedido("caixa de som", 1, 280));
-            pedido.adicionarItem(new itempedido("fone de otima qualidade", 1, 480));
-            pedido.adicionarItem(new itempedido("carregador tipo c", 1, 90));
+            pedido.adicionarItem(new ItemPedido("notebook gamer", 1, 8200));
+            pedido.adicionarItem(new ItemPedido("mouse", 2, 50));
+            pedido.adicionarItem(new ItemPedido("caixa de som", 1, 280));
+            pedido.adicionarItem(new ItemPedido("fone de otima qualidade", 1, 480));
+            pedido.adicionarItem(new ItemPedido("carregador tipo c", 1, 90));
 
             pedido.fecharPedido();
 
