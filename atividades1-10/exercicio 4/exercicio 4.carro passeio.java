ebd3c3afa7f55b112c194ed8115a4475635a882c
Just some dude <145404207+Gabrielw342@users.noreply.github.com>
2026-04-16 19:12:50 -0300

    Update exercicio 4.carro passeio.java

--- atividades1-10/exercicio 4/exercicio 4.carro passeio.java
+++ atividades1-10/exercicio 4/exercicio 4.carro passeio.java
@@ -1,1 +1,24 @@
+//mesmo tendo problemas com o override eu achp que foi de primeira
+public final class CarroPasseio extends Veiculo {
 
+    public CarroPasseio(String placa, String marca, double valorLocacaoDiaria,
+                        int anoFabricacao, double precoFipe) {
+        super(placa, marca, valorLocacaoDiaria, anoFabricacao, precoFipe);
+    }
+
+    @Override
+    public double alugarVeiculo(double pesoCarga, int dias) {
+        if (dias <= 0)
+            throw new IllegalArgumentException("valor invalido!! voce deve alugar o veiculo por pelo menos um dia");
+
+        return valorLocacaoDiaria * dias;
+    }
+
+    @Override
+    public double calcularIpva() {
+        if (isentoIpva())
+            return 0.0;
+
+        return precoFipe * 0.04;
+    }
+}
