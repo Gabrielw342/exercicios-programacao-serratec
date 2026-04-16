f1de1a943279a7ef6e157589527b03ea586ac4fd
Just some dude <145404207+Gabrielw342@users.noreply.github.com>
2026-04-16 19:16:06 -0300

    Update exercicio 4.veiculo.java

--- atividades1-10/exercicio 4/exercicio 4.veiculo.java
+++ atividades1-10/exercicio 4/exercicio 4.veiculo.java
@@ -1,61 +1,38 @@
-// so o codigo 80% feito mas eu tenho que consertar os {} e algumas outras coisas mas faco isso depois
-//agora sim eu 98% de certeza que esta tudo certo e que ele pode rodar direito
-//ultimo conserto porque so agora que eu vi que as chaves no final do codigo estavam erradas
-public class Main {
-    public static void main(String[] args) {
+//precisei rever algumas aulas mas consegui fazer o abstract e protected funcionar
+public abstract sealed class Veiculo
+implements Fretavel, Tributavel
+permits Caminhao, CarroPasseio {
 
-        bancoconta conta = new bancoconta(6665, "Vinho Diezel");
+    protected final String placa;
+    protected final String marca;
+    protected final double valorLocacaoDiaria;
+    protected final int anoFabricacao;
+    protected final double precoFipe;
 
-        conta.depositar(100);
-        conta.sacar(50);
-        conta.exibirConta();
+    public Veiculo(String placa,
+                   String marca,
+                   double valorLocacaoDiaria,
+                   int anoFabricacao,
+                   double precoFipe) {
+
+        if (placa == null || placa.isEmpty())
+            throw new IllegalArgumentException("plava nao existe");
+
+        if (valorLocacaoDiaria <= 0 || precoFipe <= 0)
+            throw new IllegalArgumentException("valor invalido");
+
+        this.placa = placa;
+        this.marca = marca;
+        this.valorLocacaoDiaria = valorLocacaoDiaria;
+        this.anoFabricacao = anoFabricacao;
+        this.precoFipe = precoFipe;
+
+    }
+
+    protected boolean isentoIpva() {
+        int anoAtual = java.time.Year.now().getValue();
+        return (anoAtual - anoFabricacao) > 20;
     }
 }
 
-class bancoconta {
 
-    private int numero;
-    private double saldo;
-    private String titular;
-
-    public bancoconta(int numero, String titular) {
-
-        // REMOVIDO: criação infinita de objetos
-        this.numero = numero;
-        this.titular = titular;
-        this.saldo = 50.0;
-
-    }
-
-    //se tudo der certo essa e a parte que faz o deposito do dinheiro na conta
-
-    public void depositar(double valor) {
-        if (valor > 0) {
-            saldo += valor;
-            System.out.println("o deposito foi um sucesso!");
-        } else {
-            System.out.println("impossivel realizer esse deposito insira um numero positivo  tente novamente");
-        }
-    }
-
-    //agora a parte de saque
-
-    public void sacar(double valor) {
-        if (valor <= 0) {
-            System.out.println("Valor invalido para saque tente outro valor por favor");
-        } else if (valor > saldo) {
-            System.out.println("saldo insoficiente tente outro valor por favor");
-        } else {
-            saldo -= valor;
-            System.out.println("saque foi realizado com sucesso!");
-        }
-    }
-
-    //agora a ultima e a parte mais facil de so checar a conta
-    public void exibirConta() {
-        System.out.println("Número: " + numero);
-        System.out.println("Titular: " + titular);
-        System.out.println("Saldo: R$ " + saldo);
-    }
-
-}
