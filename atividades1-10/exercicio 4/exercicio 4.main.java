ed5ce879363cff159fee55902eb31a6814d10117
Just some dude <145404207+Gabrielw342@users.noreply.github.com>
2026-04-16 19:18:21 -0300

    Update exercicio 4.main.java

--- atividades1-10/exercicio 4/exercicio 4.main.java
+++ atividades1-10/exercicio 4/exercicio 4.main.java
@@ -1,61 +1,33 @@
-// so o codigo 80% feito mas eu tenho que consertar os {} e algumas outras coisas mas faco isso depois
-//agora sim eu 98% de certeza que esta tudo certo e que ele pode rodar direito
-//ultimo conserto porque so agora que eu vi que as chaves no final do codigo estavam erradas
+//o codigo completo e dessa vez por um algum milagre eu acertei tudo de primeira 
 public class Main {
+
     public static void main(String[] args) {
 
-        bancoconta conta = new bancoconta(6665, "Vinho Diezel");
+        try {
+            Caminhao caminhao = new Caminhao(
+                    "ABC-1234", "VUC",
+                    500, 2012,
+                    300000, 10
+            );
 
-        conta.depositar(100);
-        conta.sacar(50);
-        conta.exibirConta();
-    }
-}
+            CarroPasseio carro = new CarroPasseio(
+                    "ODG-0284", "FUSCA",
+                    80, 1980,
+                    5000
+            );
 
-class bancoconta {
+            System.out.println("Locação caminhão: R$ " +
+                    caminhao.alugarVeiculo(12, 3));
+            System.out.println("IPVA caminhão: R$ " +
+                    caminhao.calcularIpva());
 
-    private int numero;
-    private double saldo;
-    private String titular;
+            System.out.println("\nLocação carro: R$ " +
+                    carro.alugarVeiculo(0, 5));
+            System.out.println("IPVA carro: R$ " +
+                    carro.calcularIpva());
 
-    public bancoconta(int numero, String titular) {
-
-        // REMOVIDO: criação infinita de objetos
-        this.numero = numero;
-        this.titular = titular;
-        this.saldo = 50.0;
-
-    }
-
-    //se tudo der certo essa e a parte que faz o deposito do dinheiro na conta
-
-    public void depositar(double valor) {
-        if (valor > 0) {
-            saldo += valor;
-            System.out.println("o deposito foi um sucesso!");
-        } else {
-            System.out.println("impossivel realizer esse deposito insira um numero positivo  tente novamente");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro na Atividade 4: " + e.getMessage());
         }
     }
-
-    //agora a parte de saque
-
-    public void sacar(double valor) {
-        if (valor <= 0) {
-            System.out.println("Valor invalido para saque tente outro valor por favor");
-        } else if (valor > saldo) {
-            System.out.println("saldo insoficiente tente outro valor por favor");
-        } else {
-            saldo -= valor;
-            System.out.println("saque foi realizado com sucesso!");
-        }
-    }
-
-    //agora a ultima e a parte mais facil de so checar a conta
-    public void exibirConta() {
-        System.out.println("Número: " + numero);
-        System.out.println("Titular: " + titular);
-        System.out.println("Saldo: R$ " + saldo);
-    }
-
 }
