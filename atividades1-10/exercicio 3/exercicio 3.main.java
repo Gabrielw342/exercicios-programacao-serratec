6eda340b1f66d3dff70f17116859399ff9e669ea
Just some dude <145404207+Gabrielw342@users.noreply.github.com>
2026-04-19 19:39:58 -0300

    Update exercicio 3.main.java

--- atividades1-10/exercicio 3/exercicio 3.main.java
+++ atividades1-10/exercicio 3/exercicio 3.main.java
@@ -1,17 +1,18 @@
 //so adicionando o codigo depois eu conserto
 //o mais facil de consertar so trocar algumas letras minusculas
+//ta nao e tao facil assim eu rodei o codigo e deu erro por causa de uma linha logo no inicio eu esqueci de adicionar 2 informaçoes
 public class MainAtividade3 {
 
     public static void main(String[] args) {
 
         try {
-            Cliente cliente = new Cliente("Gabriel");
+           Cliente cliente = new Cliente("Alce Barbuda", "barbudoalce005@gmail.com");
             Pedido pedido = new Pedido(cliente);
 
             pedido.adicionarItem(new ItemPedido("notebook gamer", 1, 8200));
             pedido.adicionarItem(new ItemPedido("mouse", 2, 50));
             pedido.adicionarItem(new ItemPedido("caixa de som", 1, 280));
-            pedido.adicionarItem(new ItemPedido("fone de otima qualidade", 1, 480));
+            pedido.adicionarItem(new ItemPedido("fone de otima qualidade pra dar aula",999, 280));
             pedido.adicionarItem(new ItemPedido("carregador tipo c", 1, 90));
 
             pedido.fecharPedido();
